9556f038d1d7b7912c2c06bef3534e578f351f1d
thakkar <tayeb.hakkar@edu.univ-fcomte.fr>
2022-11-28 10:33:05 +0100

    worker : working v1

--- sw.js
+++ sw.js
@@ -5,7 +5,6 @@ self.addEventListener('install', (e) => {
     e.waitUntil((async () => {
       const cache = await caches.open(cacheName);
       console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
     })());
 });
 
